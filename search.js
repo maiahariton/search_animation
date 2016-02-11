a593041047db5e6d46185d87026dc704ec6b8850
maiahariton <maia.hariton@gmail.com>
2016-02-11 18:30:37 +0800

    search animation

---
+++ search.js
@@ -0,0 +1,11 @@
+/**
+ * Created by maiahariton on 2/11/16.
+ */
+$("#inpt_search").on('focus', function() {
+    $(this).parent('label').addClass('active');
+});
+
+$("#inpt_search").on('blur', function() {
+    if ($(this).val().length == 0)
+        $(this).parent('label').removeClass('active');
+});
